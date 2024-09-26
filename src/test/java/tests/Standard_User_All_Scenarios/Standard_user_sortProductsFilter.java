3b8f36d83b58f2b0c1405dd5937266c627e11c08
Siddhartha Upadhyay <siddhartupadhya@gmail.com>
2024-09-27 02:24:27 +0530

    dry run with some minor changes

--- src/test/java/tests/Standard_User_All_Scenarios/Standard_user_sortProductsFilter.java
+++ src/test/java/tests/Standard_User_All_Scenarios/Standard_user_sortProductsFilter.java
@@ -1,11 +1,13 @@
 package tests.Standard_User_All_Scenarios;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 import tests.BaseClass;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Level;
 
 public class Standard_user_sortProductsFilter extends BaseClass {
@@ -18,14 +20,18 @@ public class Standard_user_sortProductsFilter extends BaseClass {
 
             LOGGER.log(Level.INFO, "Test Case: standard_user_order_product");
             standard_user_login.standard_user_login();
+            LOGGER.log(Level.INFO, "Selecting filter option for standard user");
+            Select select = new Select(driver.findElement(By.xpath(readConfig.getProperty("filterProductsDropdown"))));
+            select.selectByIndex(1);
+            List<WebElement> list1 = driver.findElements(By.xpath("//div[@class='inventory_item_price']"));
+            LOGGER.log(Level.INFO,list1.get(0).getText());
 
         } catch (IOException e) {
 
             throw new RuntimeException(e);
 
         }
-        LOGGER.log(Level.INFO, "Selecting filter option for standard user");
-        Select select = new Select(driver.findElement(By.xpath(readConfig.getProperty("filterProductsDropdown"))));
-        select.selectByIndex(1);
+
+
     }
 }
